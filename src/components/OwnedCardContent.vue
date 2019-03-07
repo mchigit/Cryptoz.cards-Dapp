20a51852c50874d52e53a466bca22d37ba1b6397
Ryan Price <ryanprice@users.noreply.github.com>
2019-03-06 20:34:31 -0500

    Fixed the transfer card button

--- src/components/OwnedCardContent.vue
+++ src/components/OwnedCardContent.vue
@@ -2,7 +2,7 @@
   <div>
 
 <!-- Modal -->
-<div class="modal fade" id="transferModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal fade" :id="'transfer-modal-'+id" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -62,7 +62,7 @@
               Sacrifice {{id}}
             </button>
             <div class="float-right">
-              <button class="btn btn-danger btn-gift" data-toggle="modal"  data-target="#modal">
+              <button class="btn btn-danger btn-gift" data-toggle="modal"  :data-target="'#transfer-modal-'+id">
                 <img src="static/baseline_card_giftcard_white_24dp.png" />
               </button>
             </div>
