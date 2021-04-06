7f8eff08378e7e4277bb0f16a5dce10c4fb20bb6
michael <michael.chi1997@gmail.com>
2021-04-06 09:58:20 -0400

    adding prettier integration with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     "plugin:vue/vue3-recommended",
+    "prettier",
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
   rules: {
