3fe609a1a8f8b06b2b194a0f55b20a5ce6453940
Ryan <ryanpricelondon@gmail.com>
2021-04-18 18:39:42 -0400

    Octo and fairy added

--- src/store/cardStore.js
+++ src/store/cardStore.js
@@ -5,7 +5,7 @@ const typeIdsOnChain = [];
 
 
 //push Apr 18,2021
-typeIdsOnChain.push(229,223);
+typeIdsOnChain.push(230,231,229,223);
 //push Apr 16,2021
 typeIdsOnChain.push(214,215,216,213);
 //push Apr 13,2021
